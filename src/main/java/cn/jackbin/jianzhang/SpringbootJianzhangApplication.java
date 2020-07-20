da25d727c11d13536c03f317009b73edc043ec39
SloppyJack <312883327@qq.com>
2020-07-20 21:18:13 +0800

    更新SpringBoot单元测试模块

--- src/main/java/cn/jackbin/jianzhang/SpringbootJianzhangApplication.java
+++ src/main/java/cn/jackbin/jianzhang/SpringbootJianzhangApplication.java
@@ -1,9 +1,11 @@
 package cn.jackbin.jianzhang;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cn.jackbin.jianzhang"})
+@MapperScan(basePackages = {"cn.jackbin.jianzhang"})
 public class SpringbootJianzhangApplication {
 
     public static void main(String[] args) {
