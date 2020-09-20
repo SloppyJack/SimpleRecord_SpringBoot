d9baf686840517da8e4fffee486114f6060f816e
SloppyJack <312883327@qq.com>
2020-09-20 17:24:59 +0800

    fix：修改部分注解

--- src/main/java/cn/jackbin/SimpleRecord/dto/PageDTO.java
+++ src/main/java/cn/jackbin/SimpleRecord/dto/PageDTO.java
@@ -3,8 +3,8 @@ package cn.jackbin.SimpleRecord.dto;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 
 /**
  * @author: create by bin
@@ -18,7 +18,7 @@ public class PageDTO {
      * 分页的索引
      */
     @ApiModelProperty(required = true, value = "分页的索引")
-    @NotNull(message = "当前页数须为整数")
+    @PositiveOrZero(message = "当前页数须为整数")
     int pageIndex;
 
     /**
