254fffdc58d45125a6724693d59f053295e06f1d
SloppyJack <312883327@qq.com>
2021-05-20 21:20:50 +0800

    fix：fix [issues#1] ✔

--- src/main/java/cn/jackbin/SimpleRecord/SpringbootSimpleRecordApplication.java
+++ src/main/java/cn/jackbin/SimpleRecord/SpringbootSimpleRecordApplication.java
@@ -1,17 +1,15 @@
 package cn.jackbin.SimpleRecord;
 
-import cn.jackbin.SimpleRecord.utils.SpringContextUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @MapperScan("cn.jackbin.SimpleRecord.mapper")
 public class SpringbootSimpleRecordApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(SpringbootSimpleRecordApplication.class, args);
+        SpringApplication.run(SpringbootSimpleRecordApplication.class, args);
     }
 
 }
