80b679e71de95d105b86cd1ec477c5778cf2ff6f
SloppyJack <312883327@qq.com>
2021-12-21 20:58:00 +0800

    fix：bug fix

--- src/main/java/cn/jackbin/SimpleRecord/dto/RecordAccountAnalysisDTO.java
+++ src/main/java/cn/jackbin/SimpleRecord/dto/RecordAccountAnalysisDTO.java
@@ -2,11 +2,15 @@ package cn.jackbin.SimpleRecord.dto;
 
 import cn.jackbin.SimpleRecord.entity.RecordAccountDO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 @NoArgsConstructor
 public class RecordAccountAnalysisDTO extends RecordAccountDO {
 
+    private static final long serialVersionUID = 4810168723585415780L;
     // 流入
     private Double inflow = 0.0;
 
